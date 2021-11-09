fdee97ad99219fd0eade78f030b97376354fe2fd
JhenifferFarias <jhenifferfarias.dev@gmail.com>
2021-11-09 18:38:44 -0300

    Adicionando informações de atividade

---
+++ microondas.js
@@ -0,0 +1,26 @@
+// pipoca = 10 segundos
+// macarrão = 8 egundos
+// carne = 15 segundos
+// feijão = 12 segundos
+// brigadeiro = 8 segundos
+
+function tempopadrao (tempo){
+
+  switch(tempo) {
+     case 'pipoca':
+    var tempopadrao = 10;
+    break;
+
+     case 'macarrao':
+    var tempopadrao = 8;
+    break;
+
+     case 'carne':
+    var tempopadrao = 15;
+    break;
+}
+return(tempopadrao);
+}
+
+console.log('pipoca');
+
